b290a88051a52e00a69124495fa6555818591ee4
Carlos Salazar <carfesal@espol.edu.ec>
2023-03-06 22:17:45 -0500

    finishing axe game

--- axe_game.cpp
+++ axe_game.cpp
@@ -3,8 +3,7 @@
 
 int main()
 {
-    int width = 350;
-    int length = 720;
+    /*
     bool variable = true;
     float float_variable = 5.99;
     double double_variable = 5.99;
@@ -12,18 +11,96 @@ int main()
     bool brace_initialization_2{false}; //initializaed w a value
     bool equal{ 4 == 4};
     bool not_equal{5 != 6};
+    */
 
     // loops 
     int miles = 500;
 
-    InitWindow(width,length,"Ventana Nueva");
+    int width = 720;
+    int height = 720;
 
-    while(true)
+    // circle
+    int center_x = 0;
+    int center_y = height/2;
+    int circle_radius = 20;
+    //circle edges
+    int l_circle_x{center_x - circle_radius};
+    int r_circle_x{center_x + circle_radius};
+    int u_circle_y{center_y - circle_radius};
+    int b_circle_y{center_y + circle_radius};
+
+    //rectangle or axe
+    int pos_x_r = 300;
+    int pos_y_r = 0;
+    int rectangle_length = 40;
+    int direction = 10;
+
+    //axe edges
+    int l_edge{pos_x_r};
+    int r_edge{pos_x_r + rectangle_length};
+    int u_edge{pos_y_r};
+    int b_edge{pos_y_r+rectangle_length};
+    
+    // collision 
+    bool  collision = 
+                        (r_edge >= l_circle_x) && 
+                        (u_edge <= b_circle_y) && 
+                        (b_edge >= u_circle_y) && 
+                        (l_edge <= r_circle_x);
+
+    InitWindow(width,height,"Ventana Nueva");
+    SetTargetFPS(60); // TIME OF WINDOW UPDATE EACH SECOND
+
+    while(!WindowShouldClose()) //false if x or ESC is not pressed or clicked
     {
         BeginDrawing(); // SET UP THE CANVAS IN THE WINDOW
-        ClearBackground(RED); 
+        ClearBackground(WHITE);
+
+        if(collision)
+        {
+            DrawText("Se chocaron. Se acaba el juego.", width/2, height/2, 20, BLACK);
+        } else
+        {
+            //update edges
+            l_circle_x = center_x - circle_radius;
+            r_circle_x = center_x + circle_radius;
+            u_circle_y = center_y - circle_radius;
+            b_circle_y = center_y + circle_radius;
+            
+            l_edge = pos_x_r;
+            r_edge = pos_x_r + rectangle_length;
+            u_edge = pos_y_r;
+            b_edge = pos_y_r + rectangle_length;
+
+            collision = 
+                        (r_edge >= l_circle_x) && 
+                        (u_edge <= b_circle_y) && 
+                        (b_edge >= u_circle_y) && 
+                        (l_edge <= r_circle_x);
+
+
+            DrawCircle(center_x, center_y, circle_radius, BLACK); // draw a circle (centerX, centerY, radius, Color)
+            DrawRectangle(pos_x_r,pos_y_r,rectangle_length,rectangle_length, RED); // posx and posy is the upper left corner
+
+            //moving the axe
+            pos_y_r += direction;
+            if (pos_y_r > height || pos_y_r < 0)
+            {
+                direction = -direction;
+            }
+            
+
+            if (IsKeyDown(KEY_D) && center_x < width) {
+                center_x += 10;
+            } else if (IsKeyDown(KEY_A) && center_x > 0) {
+                center_x -= 10;
+            } else if (IsKeyDown(KEY_S) && center_y < height){
+                center_y += 10;
+            } else if (IsKeyDown(KEY_W) && center_y > 0){
+                center_y -= 10;
+            }            
+        }       
+        
         EndDrawing(); // TEAR DOWN THE CANVAS
     }
-    
-    
 }
